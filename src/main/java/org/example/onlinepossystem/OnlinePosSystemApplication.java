f12e887c4ca48bf1ff7593ffccb4ec443433e3c8
matthewanton <mattantonopoulos@icloud.com>
2026-01-24 13:32:36 +0200

    Initial commit

---
+++ src/main/java/org/example/onlinepossystem/OnlinePosSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.onlinepossystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePosSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/onlinepossystem/OnlinePosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlinepossystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
