6c99dd924e9a829252f0925738f2cf4d59646017
matthewanton <mattantonopoulos@icloud.com>
2026-01-26 19:50:13 +0200

    trying to fix this login

--- src/main/java/org/example/onlinepossystem/service/CustomerUserDetailsService.java
+++ src/main/java/org/example/onlinepossystem/service/CustomerUserDetailsService.java
@@ -37,5 +37,6 @@ public class CustomerUserDetailsService implements UserDetailsService {
                 .accountLocked(false)
                 .disabled(false)
                 .build();
+        //
     }
 }
